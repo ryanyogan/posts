9d4e52f6dd979f614beeb6a4054f4fe9a6a8d3ba
Ryan Yogan <ryanyogan@gmail.com>
2014-12-22 00:14:17 -0500

    Added a post list react component

---
+++ react/components/PostList.jsx
@@ -0,0 +1,45 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React      = require('react');
+var request    = require('superagent');
+var ReactAsync = require('react-async');
+var Link       = require('react-router-component').Link;
+
+var Post = React.createClass({
+  render: function() {
+    var post = this.props.post;
+    var url = '/post/' + post._id;
+    return (
+      <div className="postBox">
+        <span><Link href={url}>{post.title}</Link></span>
+      </div>
+    );
+  }
+});
+
+var PostList = React.createClass({
+  mixins: [ReactAsync.Mixin],
+
+  getInitialStateAsync: function(callback) {
+    request.get('http://localhost:8000/api/posts', function(response) {
+      callback(null, {posts: response.body});
+    });
+  },
+  render: function() {
+    var postNodes = this.state.posts.map(function(post) {
+      return (
+        <Post key={post._id} post={post} />
+      );
+    });
+
+    return (
+      <div className="postList">
+        {postNodes}
+      </div>
+    );
+  }
+});
+
+module.exports = PostList;
