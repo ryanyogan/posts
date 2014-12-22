d6f52e84248a898f05ed936e651d81fc2c8728ad
Ryan Yogan <ryanyogan@gmail.com>
2014-12-22 00:06:23 -0500

    Added bootstrapper and main app file

---
+++ react/app.jsx
@@ -0,0 +1,35 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React     = require('react');
+var Router    = require('react-router-component');
+var Locations = Router.Locations;
+var Location  = Router.Location;
+var PostList  = require('./components/PostList.jsx');
+var PostView  = require('./components/PostView.jsx');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <html>
+        <head lang="en">
+          <meta charSet="UTF-8" />
+          <title>React App</title>
+          <link rel="stylesheeet" href="/stylesheets/style.css" />
+        </head>
+        <body>
+          <div id="main">
+            <Locations path={this.props.path}>
+              <Location path="/" handler={PostList} />
+              <Location path="/post/:id" handler={PostView} />
+            </Locations>
+          </div>
+          <script type="text/javascript" src="/scripts/react/bundle.js"></script>
+        </body>
+      </html>
+    );
+  }
+});
+
+module.exports = App;
